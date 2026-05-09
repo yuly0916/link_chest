05b019dc6a37c4402b31f9d16c06d781b47ef382
yuly0916 <yuly09162@gmail.com>
2026-05-09 23:10:37 +0900

    redirect URL 수정

--- frontend/src/page/Login/Login.jsx
+++ frontend/src/page/Login/Login.jsx
@@ -3,7 +3,7 @@ import s from './Login.module.css';
 const Login = () => {
 
     const moveToLogin = () => {
-        window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&response_type=code`;
+        window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&response_type=code&prompt=login`;
     }
     
 
